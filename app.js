6fe8aa36a90518ee30f2f34d523a68e4a7891605
Zelalem <zolabest1221@gmail.com>
2024-04-20 08:39:25 -0700

    JS-split-restaurant-bill.js  file is added.

---
+++ app.js
@@ -0,0 +1,18 @@
+// Define a function to add two numbers
+function addNumbers(a, b) {
+    return a - b;
+}
+
+// Call the function with two numbers and store the result
+var result = addNumbers(20, 7);
+
+// Log the result to the console
+console.log('The result of adding 10 and 5 is: ' + result);
+
+
+/*let firstname = "Yonathan";
+let lastname = "Makarov";
+
+console.log("First name is - " + firstname)
+console.log("Last name is: " + lastname)
+*/
--- convertTem.js
+++ convertTem.js
@@ -5,7 +5,6 @@ let fahrenheit = (originalCelsius * 9/5) + 32;
 
 let convertedCelsius = (fahrenheit - 32) * 5/9;
 
-
 console.log("Original Celsius Temperature: " + originalCelsius + " degree Celsius");
 console.log("Converted to Fahrenheit: " + fahrenheit + " degree fahrenheit");
 console.log("Converted back to Celsius: " + convertedCelsius + " degree Celsius");
